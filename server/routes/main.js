b3bc35d97bccdfa7ee54ecdf11f404c3ac4d48ac
Ayomal Weerasinghe <pasinduayomal2001@gmail.com>
2024-01-25 03:48:20 +0530

    Add locals object to index.ejs rendering

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,7 +4,12 @@ const router = express.Router();
 
 //Routes
 router.get("/", (req, res) => {
-    res.render("index.ejs");
+    const locals = {
+        title : "NodeJS Blog",
+        Description : "Simple Blog Site that created using NodeJS, Express and MongoDB for practice!"
+    }
+
+    res.render("index.ejs", {locals});
 });
 
 module.exports = router;
